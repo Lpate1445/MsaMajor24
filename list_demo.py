20d1b1574f8c4d8b79c4405e64676f5f0dcb0c20
Lpate1445 <lav.patel669@raypec.org>
2024-06-13 11:27:45 -0500

    Create list_demo.py

---
+++ list_demo.py
@@ -0,0 +1,43 @@
+def main ():
+    #list dmeo 
+    #create a list of string names 
+    names = ["John", "Mary", "Alice","Bob"]
+    list_of_integers = [10,16,24,42,14,9]
+    random_type_list = ["Cyd", 15, 22.3, "Frank"]
+
+    print(names)
+    print(list_of_integers)
+
+    #add values to a list
+    names.append ("Jonny")
+    list_of_integers.append(5)
+    list_of_integers.append(63)
+
+    print(names)
+    print(list_of_integers)
+
+    #get the number of items in a list 
+    number_of_items = len(list_of_integers)
+    print("/n/n")
+    print(f"Number of item in integer list : {number_of_items}")
+
+    #get values form our list
+
+    #get the first value from the list of integers
+    print(f"\nfirst item in integer list{list_of_integers[0]}")
+
+    #get the fourth value from the list of integers
+    print(f"\nfourth item in integer list{list_of_integers[3]}")
+
+    #print all items in the list of integers
+    print("\nInteger list items")
+    for item in list_of_integers:
+        print(item)    
+
+    print("\n")
+    number_of_items = len(list_of_integers)
+
+    for index in range(number_of_items):
+        print(f"Item {index}: {list_of_integers [index]}")
+
+main()
