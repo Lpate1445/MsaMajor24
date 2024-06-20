1b6a46f72c8225fb2ab4c71db127d599a7dec39c
Lpate1445 <lav.patel669@raypec.org>
2024-06-20 11:28:28 -0500

    Create automobile.py

---
+++ automobile.py
@@ -0,0 +1,9 @@
+
+class Automobile():
+    def __init__(self, make, model, vin, engine_size, owner, year):
+        self.make = make
+        self.model = model 
+        self.vin = vin
+        self.engine_size = engine_size
+        self.owner = owner
+        self.year = year 
